fa768348cad82895d740fa8a59e275c986fc573b
Jimmy <jimmy0328@gmail.com>
2014-10-12 20:16:32 +0800

    今日訂餐

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,7 +15,16 @@ angular.module('starter.controllers', [])
 
 //今日訂餐
 .controller('OrderCtrl', function($scope) {
-
+  $scope.storeName = "真好吃便當";
+  $scope.store_tel = "07-1234567";
+  $scope.menu_list = [
+    {id:1,name: "雞腿便當",price:"75",recommand: true},
+    {id:2,name: "豬肉便當",price:"65"},
+    {id:3,name: "炒飯",price:"55"}
+  ];
+  $scope.order = function(obj){
+    alert("你確定要訂購"+obj.name+"嗎?");
+  }
 })
 //本月消費
 .controller('ConsumerCtrl', function($scope) {
